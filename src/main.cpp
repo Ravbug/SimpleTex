e2160107c22c7ae38a63b21ccaf5ca2f8a81184b
Ravbug <ravbuganimations@gmail.com>
2022-10-02 10:47:22 -0400

    Display a PDF viewer next to the code editor

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,37 @@
 #include <QApplication>
-#include <QPushButton>
+#include <QHBoxLayout>
+#include <QMainWindow>
+#include <QFrame>
+#include <QPdfDocument>
+#include <QPdfView>
 #include <Qsci/qsciscintilla.h>
+
+class MainWindow : public QMainWindow{
+public:
+    MainWindow(QWidget* parent = nullptr) : QMainWindow(parent){
+        this->setGeometry(300, 300, 800, 400);
+        this->setWindowTitle("SimpleTex");
+        auto layout = new QHBoxLayout;
+        auto editor = new QsciScintilla;
+        editor->setText("hello");
+        editor->setUtf8(true);
+        layout->addWidget(editor);
+        
+        auto pdfView = new QPdfView;
+        layout->addWidget(pdfView);
+        
+        auto frame = new QFrame();
+        frame->setLayout(layout);
+        
+        this->setCentralWidget(frame);
+    }
+};
 
 int main(int argc, char** argv){
     QApplication a(argc, argv);
-
-    //QPushButton button ("Hello world!");
-    //button.show();
-    
-    QsciScintilla editor;
-    editor.setText("hello");
-    
-    editor.show();
+        
+    auto win = std::make_unique<MainWindow>();
+    win->show();
 
     return a.exec();
 }
